0c21eb1f785d66c44936795e92b7b089edfec87a
Amit Kumar <amitkmr@users.noreply.github.com>
2017-01-03 17:58:50 +0530

    added on click function on icon and other improvement

--- FBMobileView/background.js
+++ FBMobileView/background.js
@@ -1,29 +1,20 @@
-chrome.contextMenus.create({
-    title : "Open Facebook",
-    contexts: ["all"],
-    onclick : mobileView
-  });
-function mobileView() {
-  // Loop over all windows and their tabs
-
-  chrome.windows.getAll({ populate: true }, function(windowList) {
-    var windowFlag=true;
-    for (var i = 0; i < windowList.length; i++) {
-      if (String(windowList[i].type)=='popup') {
-          console.log(windowList[i].id)
-          // windowList[i].focus();
-          windowFlag=false;
-          chrome.windows.update(windowList[i].id,{focused:true});
-      }
-    }
-    if (windowFlag==true) {
-      chrome.windows.create({
-          url : "https://m.facebook.com/",
-          type:"popup",
-          width:400,
-          height:800,
-          left:880,
-        });
-    }
-  });
-}
+chrome.browserAction.onClicked.addListener(function() {
+      queryInfo ={}
+      queryInfo['windowType'] = "popup";
+      queryInfo['url'] = "https://*.facebook.com/*";
+      chrome.tabs.query(queryInfo, function (result){
+        if(result.length)
+        {
+          console.log(result);
+          chrome.windows.update(result[0].windowId,{focused:true});
+        }else{
+          chrome.windows.create({
+              url : "https://m.facebook.com/",
+              type:"popup",
+              width:400,
+              height:800,
+              left:880,
+            });
+        }
+      });
+});
